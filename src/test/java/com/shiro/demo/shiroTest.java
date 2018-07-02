702b474d11af4d0a227a4d6f02f12f9450bbcfc1
zhozhu85 <734837014@qq.com>
2018-07-02 22:49:46 +0800

    第一个demo

---
+++ src/test/java/com/shiro/demo/shiroTest.java
@@ -0,0 +1,30 @@
+package com.shiro.demo;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class shiroTest {
+
+    @Test
+    public void testHelloworld(){
+        
+        Factory<SecurityManager> factory=new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager  securityManager=factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        Subject subject=SecurityUtils.getSubject();
+        UsernamePasswordToken token=new UsernamePasswordToken("wang","123");
+        try {
+            subject.login(token);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Assert.assertEquals(true,subject.isAuthenticated());
+        subject.logout();
+    }
+}
